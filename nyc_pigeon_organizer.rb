31d1d828dcf062eb49fcb7cceca6573a48268e28
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-10 17:39:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,21 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_hash| 
-    value.each do |value, names|
+  data.each_with_object({}) do |(attribute, value),final_hash|
+    value.each do |inner_key, names|
       names.each do |name|
-        
+      
         if !final_hash[name]
           final_hash[name] = {}
         end
         
-        if !final_hash[name][key]
-          final_hash[name][key] = []
-         
+        if !final_hash[name][attribute]
+          final_hash[name][attribute] = []
         end
         
-        final_hash[name][key].push(value.to_s)
-        
-      end
+        final_hash[name][attribute].push(inner_key.to_s)
+      
+      end    
     end
     final_hash
   end
